a8aca0dd433106c3f0994252d1b7792138d044c7
yeunglele <yangle@cecchina.com>
2025-06-12 11:16:04 +0800

    Delete immortalwrt directory

--- immortalwrt/diy-part1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Add a feed source
-#1·在feeds.conf.default文件首行添加源码
-sed -i '1i src-git moruiris https://github.com/moruiris/openwrt-packages;immortalwrt' feeds.conf.default
-#2·在feeds.conf.default文件末尾添加源码
-#echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;immortalwrt' >>feeds.conf.default
-#3·直接在./package添加源码
-#git clone -b immortalwrt https://github.com/moruiris/openwrt-packages ./package/moruiris
